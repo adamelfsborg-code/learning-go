a4eedd2b832d00450dde6cc639dd28b06bd0144f
Adam Elfsborg <adam.elfsborg@mtek.se>
2023-09-19 05:19:15 +0200

    Complete tutorial 14

---
+++ tutorial_14/go.mod
@@ -0,0 +1,3 @@
+module github.com/adamelfsborg-code/learning-go/tutorial_14
+
+go 1.21.1
---
+++ tutorial_14/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	myBill := newBill("Elgiganten")
+
+	fmt.Printf("myBill: %v\n", myBill)
+}
